a14e8e8f5fa2a5a770c03abddd9c5f4f4c549f40
TadashiYamazaki <yama3ki.tada4@gmail.com>
2018-07-05 19:11:06 +0900

    3rd commit

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 {
   console.log("app(popup) script ready!");
-  const out = document.getElementById("out");
+  const out = document.getElementById("out1");
+  const inTxt = document.getElementById("inTxt");
   const addBtn = document.getElementById("addBtn");
+  let dataObj = {text: "", url: ""};
+  window.addEventListener("load", (ev) => {
+    console.log("window loaded");
+    chrome.runtime.sendMessage("fugafuga");
+  });
   addBtn.addEventListener("click", (ev) => {
     console.log("click!");
+    out.innerText = dataObj.url
   });
-  chrome.runtime.sendMessage("fugafuga");
-  chrome.runtime.onMessage.addListener(function(req, sender, resp) {
-    out.innerText = req;
+  chrome.runtime.onMessage.addListener((req, sender, resp) => {
+    console.log("message recived");
+    dataObj.text = req;
+    dataObj.url = sender.url;
+    inTxt.value = dataObj.url
   });
 }
--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 {
   console.log("background script ready!");
-  const extId = "jjnijkneggclocadhoblnbbnaofjajmi";
+  const extId = chrome.runtime.id;
   chrome.runtime.onMessage.addListener(function(req, sender, resp) {
     console.log(req, sender.url);
     const msg = req + " " + req;
